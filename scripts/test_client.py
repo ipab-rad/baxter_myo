cbdbb7d69693cc54940f5544ac7bb72db7d069de
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-03-24 21:04:08 +0000

    Send reasonable messages to simulate myo

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -1,26 +1,30 @@
+#!/usr/bin/python
 
-# Echo client program
 import socket
 import time
 import random
 import sys
 HOST = 'windcharger.local'    # The remote host
 PORT = 50007              # The same port as used by the server
+print("++++++++++++++++ Opening socket ++++++++++++++++")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 while 1:
     try:
-        x = float(random.randint(1, 360))
-        y = float(random.randint(1, 360))
-        z = float(random.randint(1, 360))
+        x = random.uniform(-0.2, 0.2)
+        y = random.uniform(-0.2, 0.2)
+        z = random.uniform(-0.2, 0.2)
+        o_x = random.uniform(0.0, 0.0)
+        o_y = random.uniform(-0.0, -0.0)
+        o_z = random.uniform(-0.0, 0.0)
+        # could just use joint, but *hack*
         st = "" + str(x) + " " + str(y) + " " + str(z) \
-             +  " " + str(x) + " " + str(y) + " " + str(z)
+             +  " " + str(o_x) + " " + str(o_y) + " " + str(o_z)
         print "Sending:", st
         s.sendall(st)
         data = s.recv(1024)
-
         time.sleep(1)
     except KeyboardInterrupt:
         s.close()
-        print("Socket succesfully closed")
+        print("\n++++++++++ Socket succesfully closed! ++++++++++")
         sys.exit(0)
