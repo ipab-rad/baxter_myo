e9b985cd60621cf23855082007dd06e2c1a1ae76
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-03-24 19:31:01 +0000

    Make test client send random strings

--- scripts/client_socket.py
+++ scripts/client_socket.py
@@ -1,11 +1,24 @@
 # Echo client program
 import socket
-
-HOST = 'cosmos.local'    # The remote host
+import time
+import random
+import sys
+HOST = 'windcharger.local'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+while 1:
+    try:
+        x = float(random.randint(1, 360))
+        y = float(random.randint(1, 360))
+        z = float(random.randint(1, 360))
+        st = "" + str(x) + " " + str(y) + " " + str(z)
+        print "Sending:", st
+        s.sendall(st)
+        data = s.recv(1024)
+
+        time.sleep(1)
+    except KeyboardInterrupt:
+        s.close()
+        print("Socket succesfully closed")
+        sys.exit(0)
