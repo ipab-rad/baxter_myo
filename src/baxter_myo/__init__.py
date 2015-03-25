5411e0ca2f75c4538a1d8511ba00198971e9d4d0
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-03-25 09:59:21 +0000

    Add configuration reader class to src

--- src/baxter_myo/__init__.py
+++ src/baxter_myo/__init__.py
@@ -1,1 +1,2 @@
 from server import SocketListener
+from config_reader import ConfigReader
---
+++ src/baxter_myo/config_reader.py
@@ -0,0 +1,75 @@
+import sys
+import ConfigParser
+
+import rospkg
+
+class ConfigReader(object):
+
+    def __init__(self, file):
+        """
+        `file`: string
+        """
+        self._rp = rospkg.RosPack()
+        self._config_path = self._rp.get_path('baxter_myo') \
+                            + '/config/' + file
+        self._config = ConfigParser.ConfigParser()
+        self._config.read(self._config_path)
+
+        self.left_arm = False
+        self.right_arm = False
+
+        self.left_angles = {}
+        self.right_angles = {}
+
+    def get_sections(self):
+        return self._config.sections()
+
+    def parse_general(self):
+        """
+        Returns True if parsing is succesfull, False otherwise.
+        """
+        try:
+            self.left_arm = self._config.getboolean("GENERAL",
+                                                    "left_arm")
+            self.right_arm = self._config.getboolean("GENERAL",
+                                                     "right_arm")
+        except ConfigParser.NoOptionError, err:
+            print str(err)
+            return False
+        return True
+
+    def parse_starting_pose(self):
+        """
+        Returns True if parsing is succesfull, False otherwise.
+        """
+        try:
+            self.left_angles = eval(self._config.get("STARTING_POSE",
+                                                     "left"))
+            self.right_angles = eval(self._config.get("STARTING_POSE",
+                                                      "right"))
+        except ConfigParser.NoOptionError, err:
+            print str(err)
+            return False
+        if not (type(self.right_angles) is dict and
+                type(self.left_angles) is dict):
+            print "Wrong var type in STARTING_POSE"
+            return False
+        return True
+
+    def parse_all(self):
+        results = [self.parse_general(),
+                   self.parse_starting_pose()]
+        if not all(results):
+            print "Error while parsing. Exiting!"
+            sys.exit(1)
+
+
+def main():
+    c = ConfigReader("demo_config")
+    c.parse_all()
+    print c.right_angles
+    print c.left_angles
+    print c.left_arm
+
+if __name__ == "__main__":
+    main()
