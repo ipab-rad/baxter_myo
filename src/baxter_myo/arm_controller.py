4e6ab44996a7b15664f0a4fc6160d2b23829e2b4
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-03-26 13:23:40 +0000

    Create step func, use new msg and check gripper

--- src/baxter_myo/arm_controller.py
+++ src/baxter_myo/arm_controller.py
@@ -19,46 +19,51 @@ from baxter_core_msgs.srv import (
     SolvePositionIKRequest,
 )
 
+from baxter_myo.msg import MyoData
 
 class ArmController(object):
 
-    def __init__(self, limb, starting_pos=None, push_thresh=30):
+    def __init__(self, limb, starting_pos=None, push_thresh=100):
         self.neutral_pos = starting_pos
         self.push_thresh = push_thresh
+        self.gripper_enabled = False
+        self.calibrated = False
+        self.enabled = False
+        self.data = Twist()
+
         rospy.init_node("baxter_myo")
-        rospy.Subscriber("myo_data", Twist, self.callback)
+        rospy.Subscriber("myo_data", MyoData, self.callback)
         rospy.loginfo("Subscribed to myo_data")
 
-        self.data = Twist()
         self.name_limb = limb
-        self.limb = baxter_interface.Limb(self.name_limb)
-        rospy.loginfo("Enabling Baxter...")
+        self._limb = baxter_interface.Limb(self.name_limb)
+        rospy.loginfo("Enabling Baxter")
         self._rs = baxter_interface.RobotEnable(baxter_interface.CHECK_VERSION)
         self._rs.enable()
+        self._gripper = baxter_interface.Gripper(self.name_limb)
+        self._gripper.calibrate()
         # TODO add tucking
 
         self.received = False
         self.baxter_off = Twist()
-        rospy.loginfo("Moving to starting position...")
+        rospy.loginfo("Moving to neutral position")
         self.move_to_neutral()
-        rospy.loginfo("Recording offset...")
+        rospy.loginfo("Recording offset")
         self.set_offset()
 
     def callback(self, data):
         self.received = True
-        self.data = deepcopy(data)
-        # rospy.loginfo(rospy.get_caller_id() + " heard: \
-        # \n Linear [%f, %f, %f] \
-        # \n Angular [%f, %f, %f]",
-        #               self.data.linear.x, self.data.linear.y,
-        #               self.data.linear.z, self.data.angular.x,
-        #               self.data.angular.y, self.data.angular.z)
+        self.gripper_enabled = data.gripper
+        self.calibrated = data.calibrated
+        self.enabled = data.enabled
+
+        self.data = deepcopy(data.data)
 
     def move_to_neutral(self):
-        self.limb.move_to_joint_positions(self.neutral_pos)
+        self._limb.move_to_joint_positions(self.neutral_pos)
 
     def set_offset(self):
-        pose = self.limb.endpoint_pose()
+        pose = self._limb.endpoint_pose()
         eu = tf.transformations.euler_from_quaternion(pose['orientation'])
         self.baxter_off.linear.x = pose['position'][0]
         self.baxter_off.linear.y = pose['position'][1]
@@ -68,7 +73,7 @@ class ArmController(object):
         self.baxter_off.angular.z = eu[2]
 
     def get_effort(self):
-        e = self.limb.joint_efforts()
+        e = self._limb.joint_efforts()
         s = sum([abs(e[i]) for i in e.keys()])
         return s
 
@@ -131,28 +136,37 @@ class ArmController(object):
         except:
             return None
 
-    def move_loop(self):
-        while not rospy.is_shutdown():
-            if self.received:
-                new_poss = self.find_joint_pose(
-                    self.limb.endpoint_pose(),
-                    targ_x=float(self.data.linear.x),
-                    targ_y=float(self.data.linear.y),
-                    targ_z=float(self.data.linear.z),
-                    targ_ox=float(self.data.angular.x),
-                    targ_oy=float(self.data.angular.y),
-                    targ_oz=float(self.data.angular.z))
-                rospy.loginfo("Position sent!")
-                self.limb.move_to_joint_positions(new_poss, timeout=0.1)
-                self.received = False
-                if self.is_pushing():
-                    rospy.loginfo("PUSHING!!!")
-
-
+    def step(self):
+        if not self.enabled:
+            return None
+        if not self.calibrated:
+            rospy.loginfo("Moving to initial position")
+            self.move_to_neutral()
+        elif self.received:
+            new_poss = self.find_joint_pose(
+                self._limb.endpoint_pose(),
+                targ_x=float(self.data.linear.x),
+                targ_y=float(self.data.linear.y),
+                targ_z=float(self.data.linear.z),
+                targ_ox=float(self.data.angular.x),
+                targ_oy=float(self.data.angular.y),
+                targ_oz=float(self.data.angular.z))
+            rospy.loginfo("Moving to new position")
+            self._limb.move_to_joint_positions(new_poss, timeout=0.2)
+            self.received = False
+            if self.is_pushing():
+                rospy.loginfo("PUSHING!")
+        if self.gripper_enabled:
+            rospy.loginfo("Closing gripper")
+            self._gripper.close()
+        else:
+            rospy.loginfo("Opening gripper")
+            self._gripper.open()
 
 def main():
     ac = ArmController('right')
-    ac.move_loop()
+    while 1:
+        ac.step()
 
 if __name__ == "__main__":
     sys.exit(main())
--- src/baxter_myo/server.py
+++ src/baxter_myo/server.py
@@ -9,18 +9,25 @@ from std_msgs.msg import (
     String,
 )
 
+from baxter_myo.msg import MyoData
+
 class SocketListener(object):
 
     def __init__(self, host, port, topic):
         # ROS stuff first
         rospy.init_node("myo_socket_listener")
-        self._pub = rospy.Publisher(topic, Twist)
+        self._pub = rospy.Publisher(topic, MyoData)
+        self.calibrated = False
+        self.enabled = False
+        self.gripper = False
         # networking stuff later
         self.host = host
         self.port = port
         self._socket = socket.socket(socket.AF_INET,
                                     socket.SOCK_STREAM)
-        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.setsockopt(socket.SOL_SOCKET,
+                                socket.SO_REUSEADDR,
+                                1)
         self._socket.bind((self.host, self.port))
         self._socket.listen(1)
         self._conn, self.addr = self._socket.accept()
@@ -33,26 +40,54 @@ class SocketListener(object):
             if not data:
                 break
             rospy.loginfo("Received: %s", s)
-            tw = self.prepare_data(s)
-            self._pub.publish(tw)
+            msg = self.prepare_data(s)
+            self._pub.publish(msg)
             self._conn.sendall(data)
         self._conn.close()
 
-    def prepare_data(self, s):
-        s = s[1:]
-        s = s[:-1]
-        data = s.split()
-        fs = []
-        for d in data:
-            fs.append(float(d))
+
+    def prepare_twist(self, s):
+        """
+        Returns empty Twist if s == ''
+        """
         tw = Twist()
-        tw.linear.x = fs[0]
-        tw.linear.y = fs[1]
-        tw.linear.z = fs[2]
-        tw.angular.x = fs[3]
-        tw.angular.y = fs[4]
-        tw.angular.z = fs[5]
+        if len(s) > 0:
+            s = s[1:]
+            s = s[:-1]
+            data = s.split()
+            fs = []
+            for d in data:
+                fs.append(float(d))
+            tw.linear.x = fs[0]
+            tw.linear.y = fs[1]
+            tw.linear.z = fs[2]
+            tw.angular.x = fs[3]
+            tw.angular.y = fs[4]
+            tw.angular.z = fs[5]
         return tw
+
+    def prepare_data(self, s):
+        msg = MyoData()
+        tw = self.prepare_twist('')
+
+        if s == "calibrated":
+            self.calibrated = True
+        elif s == "open_gripper":
+            self.gripper = False
+        elif s == "close_gripper":
+            self.gripper = True
+        elif s == "enable":
+            self.enabled = True
+        elif s == "disable":
+            self.enabled = False
+        else:
+            tw = self.prepare_twist(s)
+
+        msg.data = tw
+        msg.enabled = self.enabled
+        msg.calibrated = self.calibrated
+        msg.gripper = self.gripper
+        return msg
 
 
 def main():
