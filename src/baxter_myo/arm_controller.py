739f89c074f5c958533265a13a167969f003aed4
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-11-12 13:59:59 +0000

    Change main classes to deal with both arm modes

--- src/baxter_myo/arm_controller.py
+++ src/baxter_myo/arm_controller.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 
 import rospy
 from std_msgs.msg import String
@@ -9,88 +9,147 @@ from baxter_myo.pose_generator import PoseGenerator
 
 class ArmController(object):
 
-    def __init__(self, limb_name, starting_pos=None, push_thresh=10,
-                 mode='positions'):
+    def __init__(self, starting_poss=None, push_thresh=10,
+                 mode='one_arm', arm_mode='first'):
         """
         Initialises parameters and moves the arm to a neutral
         position.
         """
         self.push_thresh = push_thresh
-        self.limb_name = limb_name
-        rospy.loginfo("Creating interface and calibrating gripper")
-        self._limb = Limb(self.limb_name)
-        self._gripper = Gripper(self.limb_name, CHECK_VERSION)
-        self._gripper.calibrate()
-        self._is_fist_closed = False
-
-
-        self._neutral_pos = starting_pos
+        self._right_neutral_pos = starting_poss[0]
+        self._left_neutral_pos = starting_poss[1]
         self._mode = mode
+        self._arm_mode = arm_mode
+
+        rospy.loginfo("Creating interface and calibrating gripper")
+        self._right_limb = Limb('right')
+        self._left_limb = Limb('left')
+        self._right_gripper = Gripper('right', CHECK_VERSION)
+        self._right_gripper.calibrate()
+        self._left_gripper = Gripper('left', CHECK_VERSION)
+        self._left_gripper.calibrate()
+        self._is_right_fist_closed = False
+        self._is_left_fist_closed = False
+
         rospy.loginfo("Moving to neutral position")
         self.move_to_neutral()
         rospy.loginfo("Initialising PoseGenerator")
-        self._pg = PoseGenerator(self.limb_name,
-                                 self._mode)
-        self._sub_gesture = rospy.Subscriber("/myo_0/gesture", String,
-                                             self._gesture_callback)
+        self._pg = PoseGenerator(self._mode, self._arm_mode)
+        self._sub_right_gesture = rospy.Subscriber("/myo_0/gesture", String,
+                                                   self._right_gesture_callback)
+        self._sub_left_gesture = rospy.Subscriber("/myo_1/gesture", String,
+                                                  self._left_gesture_callback)
         self._last_data = None
-
         self._pg.calibrate()
 
-
-
     def move_to_neutral(self):
-        self._limb.move_to_joint_positions(self._neutral_pos)
+        if self._mode == "one_arm":
+            self._right_limb.move_to_joint_positions(self._right_neutral_pos)
+        elif self._mode == "two_arms":
+            self._right_limb.move_to_joint_positions(self._right_neutral_pos)
+            self._left_limb.move_to_joint_positions(self._left_neutral_pos)
+        else:
+            raise ValueError("Mode %s is invalid!" % self._mode)
 
-    def is_pushing(self):
+    def is_right_pushing(self):
         """
         Checks if any of the joints is under external stress. Returns
         true if the maximum recorded stress above specified threshold.
         """
-        e = self._limb.joint_efforts()
+        e = self._right_limb.joint_efforts()
         max_effort = max([abs(e[i]) for i in e.keys()])
         return max_effort > self.push_thresh
 
-    def _command_gripper(self):
+    def is_left_pushing(self):
+        """
+        Checks if any of the joints is under external stress. Returns
+        true if the maximum recorded stress above specified threshold.
+        """
+        e = self._left_limb.joint_efforts()
+        max_effort = max([abs(e[i]) for i in e.keys()])
+        return max_effort > self.push_thresh
+
+    def _command_right_gripper(self):
         """
         Reads state from Myo and opens/closes gripper as needed.
         """
 
-        if not self._gripper.ready():
+        if not self._right_gripper.ready():
+            return
+        if self._right_gripper.moving():
             return
 
-        if self._gripper.moving():
-            return
-
-        if self._is_fist_closed:
-            self._gripper.close()
+        if self._is_right_fist_closed:
+            self._right_gripper.close()
         else:
-            self._gripper.open()
+            self._right_gripper.open()
+
+    def _command_left_gripper(self):
+        """
+        Reads state from Myo and opens/closes gripper as needed.
+        """
+
+        if not self._left_gripper.ready():
+            return
+        if self._left_gripper.moving():
+            return
+        if self._is_left_fist_closed:
+            self._left_gripper.close()
+        else:
+            self._left_gripper.open()
 
     def step(self):
         """
         Executes a step of the main routine.
         Fist checks the status of the gripper and
         """
+        os.system('clear')
+        if self._mode == "one_arm":
+            return self.one_arm_step()
+        elif self._mode == "two_arms":
+            return self.two_arms_step()
+        else:
+            raise ValueError("Mode %s is invalid!" % self.mode)
 
-        self._command_gripper()
+    def one_arm_step(self):
+        self._command_right_gripper()
 
         pos = self._pg.generate_pose()
 
         if pos is not None:
-            if not self.is_pushing():
-                self._limb.set_joint_positions(pos)
+            if not self.is_right_pushing():
+                self._right_limb.set_joint_positions(pos)
             else:
                 rospy.logwarn("Arm is being pushed!")
         else:
             rospy.logwarn("Generated position is invalid")
 
-    def _gesture_callback(self, data):
-        self._is_fist_closed = (data is "Fist")
+
+    def two_arms_step(self):
+        self._command_right_gripper()
+        self._command_left_gripper()
+
+        poss = self._pg.generate_pose()
+
+        if poss is not None:
+            if not self.is_right_pushing():
+                self._right_limb.set_joint_positions(poss[0])
+            if not self.is_left_pushing():
+                self._left_limb.set_joint_positions(poss[1])
+            else:
+                rospy.logwarn("Arm is being pushed!")
+        else:
+            rospy.logwarn("Generated position is invalid")
+
+    def _right_gesture_callback(self, data):
+        self._is_right_fist_closed = (data is "Fist")
+
+    def _left_gesture_callback(self, data):
+        self._is_left_fist_closed = (data is "Fist")
 
 
 def main():
-    ac = ArmController('right')
+    ac = ArmController(mode='one_arm')
     while not rospy.is_shutdown():
         ac.step()
 
--- src/baxter_myo/pose_generator.py
+++ src/baxter_myo/pose_generator.py
@@ -11,11 +11,12 @@ from baxter_interface import Limb
 
 class PoseGenerator(object):
 
-    def __init__(self, limb, mode, step=5):
-        self.limb_name = limb
+    def __init__(self, mode, arm_mode, step=5):
         self.mode = mode
+        self.arm_mode = arm_mode
         self._step = step
-        self._limb = Limb(self.limb_name)
+        self._right_limb = Limb('right')
+        self._left_limb = Limb('left')
         self._subscribe()
 
     def _subscribe(self):
@@ -63,7 +64,8 @@ class PoseGenerator(object):
         raw_input("Press enter when user is at the right pose")
         self._calib_data_0 = deepcopy(self._last_data_0)
         self._calib_data_1 = deepcopy(self._last_data_1)
-        self._calib_pose = self._limb.joint_angles()
+        self._right_calib_pose = self._right_limb.joint_angles()
+        self._left_calib_pose = self._left_limb.joint_angles()
 
     def _is_vector_valid(self, data):
         """
@@ -81,43 +83,163 @@ class PoseGenerator(object):
         joint positions.
         """
 
-        os.system('clear')
+        if self.mode == "one_arm":
+            return self.one_arm_generate_pose()
+        elif self.mode == "two_arms":
+            return self.two_arms_generate_pose()
+        else:
+            raise ValueError("Mode %s is invalid!" % self.mode)
+
+    def one_arm_generate_pose(self):
         rospy.loginfo("Generating pose")
 
         data_0 = deepcopy(self._last_data_0[0])
         data_1 = deepcopy(self._last_data_1[0])
-        this_pose = deepcopy(self._calib_pose)
+        this_pose = deepcopy(self._right_calib_pose)
 
-        if self._is_vector_valid(data_0):
-            change_0 = Vector3()
-            change_0.x = data_0.x - self._calib_data_0[0].x
-            change_0.y = data_0.y - self._calib_data_0[0].y
-            change_0.z = data_0.z - self._calib_data_0[0].z
-            print "MYO_0 (Forearm)"
-            print "PITCH: ", change_0.y
-            if (self._is_over_step(change_0.y)):
-                this_pose["right_w1"] += radians(-1 * change_0.y)
-            print "YAW: ", change_0.z
-            if (self._is_over_step(change_0.z)):
-                this_pose["right_w0"] += radians(-1 * change_0.z)
-            print "ROLL: ", change_0.x
-            if (self._is_over_step(change_0.x)):
-                this_pose["right_w2"] += radians(-1 * change_0.x)
+        if self.arm_mode == "first":
+            if self._is_vector_valid(data_0):
+                change_0 = Vector3()
+                change_0.x = data_0.x - self._calib_data_0[0].x
+                change_0.y = data_0.y - self._calib_data_0[0].y
+                change_0.z = data_0.z - self._calib_data_0[0].z
+                print "MYO_0 (Forearm)"
+                print "PITCH: ", change_0.y
+                if (self._is_over_step(change_0.y)):
+                    this_pose["right_w1"] += radians(-1 * change_0.y)
+                print "YAW: ", change_0.z
+                if (self._is_over_step(change_0.z)):
+                    this_pose["right_w0"] += radians(-1 * change_0.z)
+                print "ROLL: ", change_0.x
+                if (self._is_over_step(change_0.x)):
+                    this_pose["right_w2"] += radians(-1 * change_0.x)
 
-        if self._is_vector_valid(data_1):
-            change_1 = Vector3()
-            change_1.x = data_1.x - self._calib_data_1[0].x
-            change_1.y = data_1.y - self._calib_data_1[0].y
-            change_1.z = data_1.z - self._calib_data_1[0].z
-            print "MYO_1 (Upper arm)"
-            print "PITCH: ", change_1.y
-            if (self._is_over_step(change_1.y)):
-                this_pose["right_e1"] += radians(change_1.y)
-            print "YAW: ", change_1.z
-            if (self._is_over_step(change_1.z)):
-                this_pose["right_e0"] += radians(change_1.z)
-            print "ROLL: ", change_1.x
-            if (self._is_over_step(change_1.x)):
-                this_pose["right_s0"] += radians(change_1.x)
+            if self._is_vector_valid(data_1):
+                change_1 = Vector3()
+                change_1.x = data_1.x - self._calib_data_1[0].x
+                change_1.y = data_1.y - self._calib_data_1[0].y
+                change_1.z = data_1.z - self._calib_data_1[0].z
+                print "MYO_1 (Upper arm)"
+                print "PITCH: ", change_1.y
+                if (self._is_over_step(change_1.y)):
+                    this_pose["right_e1"] += radians(change_1.y)
+                print "YAW: ", change_1.z
+                if (self._is_over_step(change_1.z)):
+                    this_pose["right_e0"] += radians(change_1.z)
+                print "ROLL: ", change_1.x
+                if (self._is_over_step(change_1.x)):
+                    this_pose["right_s0"] += radians(change_1.x)
+
+        elif self.arm_mode == "second":
+            print "SECOND!"
+
+            # Alex you need to change below
+            if self._is_vector_valid(data_1):
+                change_1 = Vector3()
+                change_1.x = data_1.x - self._calib_data_1[0].x
+                change_1.y = data_1.y - self._calib_data_1[0].y
+                change_1.z = data_1.z - self._calib_data_1[0].z
+                print "MYO_1 (Upper arm)"
+                print "PITCH: ", data_1.y
+                if (self._is_over_step(change_1.y)):
+                    this_pose["right_s1"] += radians(-1 * change_1.y)
+                print "YAW: ", change_1.z
+                if (self._is_over_step(change_1.z)):
+                    this_pose["right_s0"] += radians(change_1.z)
+                print "ROLL: ", change_1.x
+                if (self._is_over_step(change_1.x)):
+                    this_pose["right_e0"] += radians(change_1.x)
+            if self._is_vector_valid(data_0):
+                change_0 = Vector3()
+                change_0.x = data_0.x - self._calib_data_0[0].x
+                change_0.y = data_0.y - self._calib_data_0[0].y
+                change_0.z = data_0.z - self._calib_data_0[0].z
+                print "MYO_0 (Forearm)"
+                print "PITCH: ", data_0.y
+                if (self._is_over_step(change_0.y)):
+                    change_0.y += change_1.y
+                    this_pose["right_e1"] += radians(-1 * change_0.y)
+                print "YAW: ", data_0.z
+                if (self._is_over_step(change_0.z)):
+                    this_pose["right_w1"] += radians(change_0.z)
+                print "ROLL: ", data_0.x
+                if (self._is_over_step(change_0.x)):
+                    this_pose["right_w2"] += radians(-1 * change_0.x)
 
         return this_pose
+
+    def two_arms_generate_pose(self):
+        rospy.loginfo("Generating pose")
+
+        data_0 = deepcopy(self._last_data_0[0])
+        data_1 = deepcopy(self._last_data_1[0])
+        right_pose = deepcopy(self._right_calib_pose)
+        left_pose = deepcopy(self._left_calib_pose)
+
+        if self.arm_mode == "first":
+            if self._is_vector_valid(data_0):
+                change_0 = Vector3()
+                change_0.x = data_0.x - self._calib_data_0[0].x
+                change_0.y = data_0.y - self._calib_data_0[0].y
+                change_0.z = data_0.z - self._calib_data_0[0].z
+                print "MYO_0 (Right forearm)"
+                print "PITCH: ", change_0.y
+                if (self._is_over_step(change_0.y)):
+                    right_pose["right_w1"] += radians(-1 * change_0.y)
+                print "YAW: ", change_0.z
+                if (self._is_over_step(change_0.z)):
+                    right_pose["right_w0"] += radians(-1 * change_0.z)
+                print "ROLL: ", change_0.x
+                if (self._is_over_step(change_0.x)):
+                    right_pose["right_w2"] += radians(-1 * change_0.x)
+
+            if self._is_vector_valid(data_1):
+                change_1 = Vector3()
+                change_1.x = data_1.x - self._calib_data_1[0].x
+                change_1.y = data_1.y - self._calib_data_1[0].y
+                change_1.z = data_1.z - self._calib_data_1[0].z
+                print "MYO_1 (Left forearm)"
+                print "PITCH: ", change_1.y
+                if (self._is_over_step(change_1.y)):
+                    left_pose["left_w1"] += radians(-1 * change_1.y)
+                print "YAW: ", change_1.z
+                if (self._is_over_step(change_1.z)):
+                    left_pose["left_w0"] += radians(-1 * change_1.z)
+                print "ROLL: ", change_1.x
+                if (self._is_over_step(change_1.x)):
+                    left_pose["left_w2"] += radians(-1 * change_1.x)
+
+        elif self.arm_mode == "second":
+            if self._is_vector_valid(data_0):
+                change_0 = Vector3()
+                change_0.x = data_0.x - self._calib_data_0[0].x
+                change_0.y = data_0.y - self._calib_data_0[0].y
+                change_0.z = data_0.z - self._calib_data_0[0].z
+                print "MYO_0 (Right forearm)"
+                print "PITCH: ", change_0.y
+                if (self._is_over_step(change_0.y)):
+                    right_pose["right_e1"] += radians(change_0.y)
+                print "YAW: ", change_0.z
+                if (self._is_over_step(change_0.z)):
+                    right_pose["right_w1"] += radians(change_0.z)
+                print "ROLL: ", change_0.x
+                if (self._is_over_step(change_0.x)):
+                    right_pose["right_w2"] += radians(-1 * change_0.x)
+
+            if self._is_vector_valid(data_1):
+                change_1 = Vector3()
+                change_1.x = data_1.x - self._calib_data_1[0].x
+                change_1.y = data_1.y - self._calib_data_1[0].y
+                change_1.z = data_1.z - self._calib_data_1[0].z
+                print "MYO_1 (Left forearm)"
+                print "PITCH: ", change_1.y
+                if (self._is_over_step(change_1.y)):
+                    left_pose["left_e1"] += radians(-1 * change_1.y)
+                print "YAW: ", change_1.z
+                if (self._is_over_step(change_1.z)):
+                    left_pose["left_w1"] += radians(-1 * change_1.z)
+                print "ROLL: ", change_1.x
+                if (self._is_over_step(change_1.x)):
+                    left_pose["left_w2"] += radians(change_1.x)
+
+        return (right_pose, left_pose)
